fa105cba3390bf89d690a39a00024ae424b5fa16
choudhary09srishti-boop <choudhary09srishti@gmail.com>
2026-04-16 22:59:21 +0530

    main file update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import os
 from admin_panel.app import app
 import api.routes
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=False)
